8aca020f93e2279cbeb342f33da514958e6ccbd1
seahopark <afloat@outlook.com>
2017-03-06 01:15:12 +0900

    Start
    Start Learning

---
+++ test.py
@@ -0,0 +1,32 @@
+# 수와 계산 형식
+print (10 + 5)
+print (10 - 5)
+print (10 / 5)
+print (10 * 5)
+
+#문자열 표현
+print ('Hello')
+print ("hello")
+print ("hello World!")
+print ("hello 'World!'")
+#----------
+print ('hello ' '+' 'World!')
+print ('hello '*3)
+print ('hello '[0])
+print ('hello '[1])
+print ('hello '[2])
+#----------
+print ('hello world' .capitalize())
+print ('hello world' .upper())
+print ('hello world' .__len__())
+print (len('hello world'))
+print ('hello world' .replace('world','programming'))
+#------------
+print("egoing's \"tutorial\"")
+print("\\")
+print("Hello\nworld")
+print("Hello\tworld")
+print('Hello\tworld')
+#------------
+print (10 + 5)
+print ("10" + "5")
