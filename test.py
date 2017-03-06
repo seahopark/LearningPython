8ae8c246033d6e829c020b23bacb5ca8cf78b74e
seahopark <afloat@outlook.com>
2017-03-07 00:17:20 +0900

    변수 공부 업데이트
    변수 업데이트

--- test.py
+++ test.py
@@ -34,3 +34,25 @@ print('Hello\tworld')
 #------------
 print (10 + 5)
 print ("10" + "5")
+
+#변수
+x=10
+y=5
+print (x+y)
+
+title= "python"
+print("Title is "+title)
+
+#------------
+#안녕하세요 {username}님,
+#지금부터 {username}님을 위한 클라스를 시작할게요 구현
+username="{username}"
+print("안녕하세요 "+ username +"님")
+print("지금부터 "+ username +"님을 위한 클라스를 시작할게요")
+
+#------------
+#수학계산 적용
+x = 5.5
+y = 100
+z = y*(10.00-x)
+print(z)
