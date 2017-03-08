6d1cca5cf566769efc603a86874a7eadf89d84fb
seahopark <afloat@outlook.com>
2017-03-09 00:25:46 +0900

    Logical Operator Update
    update

---
+++ Logical_Operator/and.py
@@ -0,0 +1,20 @@
+#input_id = input("아이디를 입력해 주세요.\n")
+#input_pw = input("비밀번호를 입력해 주세요.\n")
+#real_id = "12"
+#real_pw = "12"
+#if real_id == input_id:
+#    if real_pw == input_pw:
+#        print("Hello!")
+#    else:
+#        print("Sorry!")
+#else:
+#    print("Sorry!")
+
+input_id = input("아이디를 입력해 주세요.\n")
+input_pw = input("비밀번호를 입력해 주세요.\n")
+real_id = "12"
+real_pw = "12"
+if real_id == input_id and real_pw == input_pw:
+        print("Hello!")
+else:
+    print("Sorry!")
