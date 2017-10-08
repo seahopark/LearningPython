b0d406ca85ed438568f306fa51b585a2fca67350
Antony Park | 박세호 <afloat@outlook.com>
2017-10-09 02:44:19 +0900

    1
    12

---
+++ test2.py
@@ -0,0 +1,7 @@
+user_data1 = input()
+user_data2 = input()
+user_data_opt = input()
+
+user_data_total = int(user_data1) + int(user_data2) + int(user_data_opt)
+
+print(user_data_total)
---
+++ turtle.py
@@ -0,0 +1,6 @@
+import turtle
+
+turle.shape("turle")
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
